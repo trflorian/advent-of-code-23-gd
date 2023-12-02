96a01aea3e39e1b1e3f37d1470db3407ed579cd3
trflorian <florian.trautweiler@hispeed.ch>
2023-12-02 10:33:40 +0100

    part 2 of puzzle day 1

--- days/day01.gd
+++ days/day01.gd
@@ -9,6 +9,25 @@ func run(input: String) -> String:
 
 func process_line(line: String):
 	var digits: Array[int] = []
+	
+	
+	var spelled_out_digits = {
+		"one": 1,
+		"two": 2,
+		"three": 3,
+		"four": 4,
+		"five": 5,
+		"six": 6,
+		"seven": 7,
+		"eight": 8,
+		"nine": 9
+	}
+	
+	for sd in spelled_out_digits.keys():
+		var digit: int = spelled_out_digits[sd]
+		# eightwothree -> eight8eighttwo2two
+		line = line.replace(sd, sd + str(digit) + sd)
+	
 	for char in line:
 		if char.is_valid_int():
 			digits.append(char.to_int())
