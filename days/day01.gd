6e86e969e78356d8ec475f53eb19b48e7ffdf3b5
trflorian <florian.trautweiler@hispeed.ch>
2023-12-02 22:16:51 +0100

    load full data script

--- days/day01.gd
+++ days/day01.gd
@@ -3,6 +3,8 @@ extends Node
 func run(input: String) -> String:
 	var output: int = 0
 	for line in input.split("\n"):
+		if line.replace(" ", "").is_empty():
+			continue
 		var digits = process_line(line)
 		output += digits[0] * 10 + digits[1]
 	return str(output)
--- main.gd
+++ main.gd
@@ -6,13 +6,22 @@ extends Control
 @export var button_run: Button
 @export var line_edit_output: LineEdit
 
+@export var http_request: HTTPRequest
+
 func _ready() -> void:
 	button_load_input.button_down.connect(_on_load_input)
 	button_run.button_down.connect(_on_run)
+	http_request.request_completed.connect(_on_data_loaded)
 
 func _on_load_input() -> void:
-	print("loading data...")
-	
+	var day = 1
+	http_request.request("https://adventofcode.com/2023/day/%d/input" % day, [
+		"Cookie: session=53616c7465645f5fa098c80954405e1b1ca70cd0d51b2105e578f9e808506589bb2127e5951b655ba555e9cc1127ca207e4e60a7d36387f503f0aa8c36c63fc4"
+	])
+
+func _on_data_loaded(result, response_code, headers, body) -> void:
+	text_edit_input.text = body.get_string_from_utf8()
+
 func _on_run() -> void:
 	var script = preload("res://days/day01.gd").new()
 	var output = script.run(text_edit_input.text)
